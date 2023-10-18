781e64c831131ec66344897794e08b267009ff8d
Gariel Eusse <gabriel.eusse96@gmail.com>
2023-10-18 10:55:18 -0500

    create message function

--- mealGenerator.js
+++ mealGenerator.js
@@ -8,3 +8,8 @@ function randomNumber(max) {
     return Math.floor(Math.random() * max);
 }
 
+let protein = meal.protein[randomNumber(meal.protein.length)];
+let salad = meal.salad[randomNumber(meal.salad.length)];
+let carb = meal.carb[randomNumber(meal.carb.length)];
+
+console.log (`Your Balanced meal for today is composed of 200 grams of ${protein}, 200 grams of ${salad} salad and 100 grams of ${carb}.`)
