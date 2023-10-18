132795db4c9432193c009243e9aab53e30b4c3f1
Gariel Eusse <gabriel.eusse96@gmail.com>
2023-10-18 10:38:22 -0500

    add structured data

---
+++ mealGenerator.js
@@ -0,0 +1,10 @@
+let meal = {
+    protein : ["Chicken", "Beef", "Pork", "Lamb", "Fish"],
+    salad : ["Nicoise", "Caesar", "Cobb", "Greek", "Tabbouleh", "Coleslaw", "Caprese" ],
+    carb : ["White Rice", "Pasta", "Baked Potatoes", "French Fries"]
+}
+
+function randomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+
